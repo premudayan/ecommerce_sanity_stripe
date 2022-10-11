cfdaaa321843e3da9ac7ee3a33d05b0febb111e9
Prem Pillai Udayan <prem_udayan@yahoo.com>
2022-10-10 21:56:37 -0400

    Update StateContext.js
    Update toggleCartItemQuanitity function which would solve the jumping issue in the cart when updating the quantity

--- context/StateContext.js
+++ context/StateContext.js
@@ -46,22 +46,29 @@ export const StateContext = ({ children }) => {
     setCartItems(newCartItems);
   }
 
-  const toggleCartItemQuanitity = (id, value) => {
-    foundProduct = cartItems.find((item) => item._id === id)
-    index = cartItems.findIndex((product) => product._id === id);
-    const newCartItems = cartItems.filter((item) => item._id !== id)
-
-    if(value === 'inc') {
-      setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity + 1 } ]);
-      setTotalPrice((prevTotalPrice) => prevTotalPrice + foundProduct.price)
-      setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
-    } else if(value === 'dec') {
-      if (foundProduct.quantity > 1) {
-        setCartItems([...newCartItems, { ...foundProduct, quantity: foundProduct.quantity - 1 } ]);
-        setTotalPrice((prevTotalPrice) => prevTotalPrice - foundProduct.price)
-        setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
-      }
-    }
+ const toggleCartItemQuanitity = (id, value) => {
+    // This would solve the jumping issue in the cart when updating the quantity
+    setCartItems(prevState => {
+      const tempCartItems = prevState.map(item => {
+        if (item._id === id ) {
+          if ( value === 'inc' ) {
+            setTotalPrice((prevTotalPrice) => prevTotalPrice + item.price)
+            setTotalQuantities(prevTotalQuantities => prevTotalQuantities + 1)
+            return {...item, quantity: item.quantity + 1};
+          } else if ( value === 'dec' ) {
+            if (item.quantity > 1) {
+              setTotalPrice((prevTotalPrice) => prevTotalPrice - item.price)
+              setTotalQuantities(prevTotalQuantities => prevTotalQuantities - 1)
+              return {...item, quantity: item.quantity - 1};
+            } else  {
+              return item;
+            }
+          }
+        }
+        return item;
+      });
+      return tempCartItems;
+    });
   }
 
   const incQty = () => {
